c599b191a9c6d88a48edf9283fd4fdbc88fd4471
shiyufeng <452703041@qq.com>
2020-07-24 11:26:33 +0800

    update product

--- src/abs/middleground/business/production/manager/__init__.py
+++ src/abs/middleground/business/production/manager/__init__.py
@@ -4,32 +4,32 @@ from infrastructure.core.exception.business_error import BusinessError
 from infrastructure.utils.common.split_page import Splitor
 
 from abs.common.manager import BaseManager
-from abs.middleground.business.production.models import Brand, Production
+from abs.middleground.business.production.models import Brand,Production
 
 
 class ProductionServer(BaseManager):
 
     @classmethod
-    def get(cls, production_id):
-        production = Production.get_byid(production_id)
+    def get(cls,production_id):
+        production=Production.get_byid(production_id)
         if production is None:
             raise BusinessError("产品不存在")
         return production
 
     @classmethod
-    def search(cls, current_page, company_id, **search_info):
-        production_qs = Production.query(
+    def search(cls,current_page,company_id,**search_info):
+        production_qs=Production.query(
             **search_info
         ).filter(
             company_id=company_id
         )
-        splitor = Splitor(current_page, production_qs)
+        splitor=Splitor(current_page,production_qs)
         return splitor
 
     @classmethod
-    def generate(cls, company_id, brand_id, **production_info):
-        brand = cls.get_brand(brand_id)
-        production = Production.create(
+    def generate(cls,company_id,brand_id,**production_info):
+        brand=cls.get_brand(brand_id)
+        production=Production.create(
             company_id=company_id,
             brand=brand,
             **production_info
@@ -37,45 +37,77 @@ class ProductionServer(BaseManager):
         return production
 
     @classmethod
-    def update(cls, production_id, **production_info):
-        production = cls.get(production_id)
+    def update(cls,production_id,**production_info):
+        production=cls.get(production_id)
         production.update(
             **production_info
         )
         return True
 
     @classmethod
-    def generate_brand(cls, company_id, **brand_info):
-        brand = Brand.create(
+    def delete(cls,product_id):
+        product=cls.get(brand_id)
+        # 未添加判断是否可删除的条件
+        product.delete()
+        return True
+
+    @classmethod
+    def generate_brand(cls,company_id,**brand_info):
+        cls.is_brand_exsited(company_id,brand_info["name"])
+        brand=Brand.create(
             company_id=company_id,
             **brand_info
         )
         return brand
 
     @classmethod
-    def get_brand(cls, brand_id):
-        brand = Brand.get_byid(
+    def get_brand(cls,brand_id):
+        brand=Brand.get_byid(
             brand_id,
         )
         return brand
 
     @classmethod
-    def search_brand(cls, current_page, company_id, **search_info):
-        brand_qs = Brand.query(
+    def search_brand(cls,current_page,company_id,**search_info):
+        brand_qs=Brand.query(
             **search_info
         ).filter(
             company_id=company_id
-        )
-        splitor = Splitor(current_page, brand_qs)
+        ).order_by("-create_time")
+        splitor=Splitor(current_page,brand_qs)
         return splitor
 
     @classmethod
-    def update_brand(cls, brand_id, **brand_info):
-        brand = cls.get_brand(brand_id)
+    def update_brand(cls,brand_id,**brand_info):
+        brand=cls.get_brand(brand_id)
         if brand is None:
             raise BusinessError("未知的品牌")
-
+        cls.is_brand_exsited(brand.company_id,brand_info["name"],brand)
         brand.update(
             **brand_info
         )
         return brand
+
+    @classmethod
+    def is_brand_exsited(cls,company_id,name,brand=None):
+        brand_qs=Brand.query(
+            company_id=company_id,
+            name=name
+        )
+        if brand is not None:
+            brand_qs=brand_qs.exclude(id=brand.id)
+        if brand_qs.count()>0:
+            raise BusinessError("品牌名称重复")
+
+    @classmethod
+    def delete_brand(cls,brand_id):
+        brand=cls.get_brand(brand_id)
+        if brand is None:
+            raise BusinessError("未知的品牌")
+        product_qs=Production.query(
+            brand=brand
+        )
+        if product_qs.count()>0:
+            raise BusinessError("此品牌存在产品无法删除")
+        brand.delete()
+        return True
--- src/agile/crm/apis/production/__init__.py
+++ src/agile/crm/apis/production/__init__.py
@@ -1,11 +1,11 @@
 # coding=UTF-8
 
 import json
-from infrastructure.core.field.base import CharField, DictField, \
-        IntField, ListField, DatetimeField
+from infrastructure.core.field.base import CharField,DictField,\
+        IntField,ListField,DatetimeField
 from infrastructure.core.api.utils import with_metaclass
-from infrastructure.core.api.request import RequestField, RequestFieldSet
-from infrastructure.core.api.response import ResponseField, ResponseFieldSet
+from infrastructure.core.api.request import RequestField,RequestFieldSet
+from infrastructure.core.api.response import ResponseField,ResponseFieldSet
 
 from agile.crm.manager.api import StaffAuthorizedApi
 from abs.middleground.business.production.manager import ProductionServer
@@ -14,11 +14,11 @@ from abs.middleground.business.enterprise.manager import EnterpriseServer
 
 class Get(StaffAuthorizedApi):
 
-    request = with_metaclass(RequestFieldSet)
-    request.production_id = RequestField(IntField, desc="产品id")
+    request=with_metaclass(RequestFieldSet)
+    request.production_id=RequestField(IntField,desc="产品id")
 
-    response = with_metaclass(ResponseFieldSet)
-    response.production_info = ResponseField(
+    response=with_metaclass(ResponseFieldSet)
+    response.production_info=ResponseField(
         DictField,
         desc="产品信息",
         conf={
@@ -70,14 +70,14 @@ class Get(StaffAuthorizedApi):
     def get_author(cls):
         return "Roy"
 
-    def execute(self, request):
-        production = ProductionServer.get(
+    def execute(self,request):
+        production=ProductionServer.get(
             request.production_id
         )
         return production
 
-    def fill(self, response, production):
-        response.production_info = {
+    def fill(self,response,production):
+        response.production_info={
             'id': production.id,
             'name': production.name,
             'description': production.description,
@@ -93,19 +93,20 @@ class Get(StaffAuthorizedApi):
 
 
 class Search(StaffAuthorizedApi):
-    request = with_metaclass(RequestFieldSet)
-    request.current_page = RequestField(IntField, desc="当前页面")
-    request.search_info = RequestField(
+    request=with_metaclass(RequestFieldSet)
+    request.current_page=RequestField(IntField,desc="当前页面")
+    request.search_info=RequestField(
         DictField,
         desc="搜索产品",
         conf={
+              'name': CharField(desc="产品名称",is_required=False),
         }
     )
 
-    response = with_metaclass(ResponseFieldSet)
-    response.total = ResponseField(IntField, desc="数据总数")
-    response.total_page = ResponseField(IntField, desc="总页码数")
-    response.data_list = ResponseField(
+    response=with_metaclass(ResponseFieldSet)
+    response.total=ResponseField(IntField,desc="数据总数")
+    response.total_page=ResponseField(IntField,desc="总页码数")
+    response.data_list=ResponseField(
         ListField,
         desc="产品列表",
         fmt=DictField(
@@ -160,17 +161,17 @@ class Search(StaffAuthorizedApi):
     def get_author(cls):
         return "Roy"
 
-    def execute(self, request):
-        company = EnterpriseServer.get_crm__company()
-        spliter = ProductionServer.search(
+    def execute(self,request):
+        company=EnterpriseServer.get_crm__company()
+        spliter=ProductionServer.search(
             request.current_page,
             company.id,
             **request.search_info
         )
         return spliter
 
-    def fill(self, response, production_spliter):
-        data_list = [{
+    def fill(self,response,production_spliter):
+        data_list=[{
             'id': production.id,
             'name': production.name,
             'description': production.description,
@@ -182,16 +183,16 @@ class Search(StaffAuthorizedApi):
             'update_time': production.update_time,
             'create_time': production.create_time,
         } for production in production_spliter.data]
-        response.data_list = data_list
-        response.total = production_spliter.total
-        response.total_page = production_spliter.total_page
+        response.data_list=data_list
+        response.total=production_spliter.total
+        response.total_page=production_spliter.total_page
         return response
 
 
 class Add(StaffAuthorizedApi):
-    request = with_metaclass(RequestFieldSet)
-    request.brand_id = RequestField(IntField, desc="品牌Id")
-    request.production_info = RequestField(
+    request=with_metaclass(RequestFieldSet)
+    request.brand_id=RequestField(IntField,desc="品牌Id")
+    request.production_info=RequestField(
         DictField,
         desc="产品信息",
         conf={
@@ -229,8 +230,8 @@ class Add(StaffAuthorizedApi):
         }
     )
 
-    response = with_metaclass(ResponseFieldSet)
-    response.production_id = ResponseField(IntField, desc="产品ID")
+    response=with_metaclass(ResponseFieldSet)
+    response.production_id=ResponseField(IntField,desc="产品ID")
 
     @classmethod
     def get_desc(cls):
@@ -240,27 +241,27 @@ class Add(StaffAuthorizedApi):
     def get_author(cls):
         return "Roy"
 
-    def execute(self, request):
-        company = EnterpriseServer.get_crm__company()
-        add_info = request.production_info
-        add_info.attribute_list = json.dumps(add_info.attribute_list)
-        add_info.workflow_list = json.dumps(add_info.workflow_list)
-        production = ProductionServer.generate(
+    def execute(self,request):
+        company=EnterpriseServer.get_crm__company()
+        add_info=request.production_info
+        add_info.attribute_list=json.dumps(add_info.attribute_list)
+        add_info.workflow_list=json.dumps(add_info.workflow_list)
+        production=ProductionServer.generate(
             company_id=company.id,
             brand_id=request.brand_id,
             **add_info
         )
         return production
 
-    def fill(self, response, production):
-        response.production_id = production.id
+    def fill(self,response,production):
+        response.production_id=production.id
         return response
 
 
 class Update(StaffAuthorizedApi):
-    request = with_metaclass(RequestFieldSet)
-    request.production_id = RequestField(IntField, desc="产品id")
-    request.update_info = RequestField(
+    request=with_metaclass(RequestFieldSet)
+    request.production_id=RequestField(IntField,desc="产品id")
+    request.update_info=RequestField(
         DictField,
         desc="需要更新的产品信息",
         conf={
@@ -298,7 +299,7 @@ class Update(StaffAuthorizedApi):
         }
     )
 
-    response = with_metaclass(ResponseFieldSet)
+    response=with_metaclass(ResponseFieldSet)
 
     @classmethod
     def get_desc(cls):
@@ -308,14 +309,39 @@ class Update(StaffAuthorizedApi):
     def get_author(cls):
         return "Roy"
 
-    def execute(self, request):
-        update_info = request.update_info
-        update_info.attribute_list = json.dumps(update_info.attribute_list)
-        update_info.workflow_list = json.dumps(update_info.workflow_list)
+    def execute(self,request):
+        update_info=request.update_info
+        update_info.attribute_list=json.dumps(update_info.attribute_list)
+        update_info.workflow_list=json.dumps(update_info.workflow_list)
         ProductionServer.update(
             production_id=request.production_id,
             **update_info
         )
 
-    def fill(self, response, address_list):
+    def fill(self,response,address_list):
+        return response
+
+
+class Remove(StaffAuthorizedApi):
+    """删除产品信息"""
+    request=with_metaclass(RequestFieldSet)
+    request.brand_id=RequestField(IntField,desc="产品id")
+
+
+    response=with_metaclass(ResponseFieldSet)
+
+    @classmethod
+    def get_desc(cls):
+        return "产品信息删除接口"
+
+    @classmethod
+    def get_author(cls):
+        return "Fsy"
+
+    def execute(self,request):
+        ProductionServer.delete_brand(
+            request.brand_id,
+        )
+
+    def fill(self,response):
         return response
