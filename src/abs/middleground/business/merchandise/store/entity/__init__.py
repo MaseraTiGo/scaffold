05a25dabd214f974a4ba23c5b11a5fc023102960
Burgess Yang <237818280@qq.com>
2020-07-29 14:36:21 +0800

    update merchandise path

--- src/abs/middleground/business/merchandise/store/entity/__init__.py
+++ src/abs/middleground/business/merchandise/store/entity/__init__.py
@@ -1,6 +1,6 @@
 # coding=UTF-8
 
 
-from abs.middleground.business.merchandise.store.entity.merchanise import Merchandise
+from abs.middleground.business.merchandise.store.entity.merchandise import Merchandise
 from abs.middleground.business.merchandise.store.entity.specification import \
         Specification, SpecificationValue
