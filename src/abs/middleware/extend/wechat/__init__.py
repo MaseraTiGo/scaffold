6341b8afca15dea6a8d23e558a5e8fb4d8ae606e
xiongyicheng <369874203@qq.com>
2020-08-04 21:04:54 +0800

    update

--- src/abs/middleware/extend/wechat/__init__.py
+++ src/abs/middleware/extend/wechat/__init__.py
@@ -65,7 +65,7 @@ class MiniMch(object):
 
     @property
     def notify_url(self):
-        return ' http://3tzeva.natappfree.cc'
+        return 'http://d8x4uj.natappfree.cc'
 
     @property
     def key(self):
