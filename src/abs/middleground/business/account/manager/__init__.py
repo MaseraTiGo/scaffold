3345ebb07f630321332b78e0b100ef812635e234
Burgess Yang <237818280@qq.com>
2020-08-07 12:07:07 +0800

    add to logout interface

--- src/abs/middleground/business/account/manager/__init__.py
+++ src/abs/middleground/business/account/manager/__init__.py
@@ -8,43 +8,20 @@ from abs.middleground.business.account.utils.constant import StatusTypes
 
 class AccountServer(BaseManager):
 
-    APPLY_CLS=None
+    APPLY_CLS = None
 
     @classmethod
-    def create(cls,role_id,username,password):
-        account=cls.APPLY_CLS.get_byrole(role_id=role_id)
+    def create(cls, role_id, username, password):
+        account = cls.APPLY_CLS.get_byrole(role_id=role_id)
         if account is not None:
             raise BusinessError('客户已存在，不能创建账号！')
 
-        account=cls.APPLY_CLS.create(
+        account = cls.APPLY_CLS.create(
             role_id=role_id,
             username=username,
             password=password,
             status=StatusTypes.ENABLE
         )
-        token=TokenManager.generate_token(
-            account.role_type,
-            account.role_id
-        )
-        return token
-
-    @classmethod
-    def renew_token(cls,auth_str,renew_str):
-        return TokenManager.renew_token(auth_str,renew_str)
-
-    @classmethod
-    def login(cls,username,password):
-        is_exsited,account=cls.APPLY_CLS.is_exsited(username,password)
-        if is_exsited and password:
-            token=TokenManager.generate_token(
-                account.role_type,
-                account.role_id
-            )
-            return token
-        raise BusinessError('账号或密码错误！')
-
-    @classmethod
-    def account_login(cls, account):
         token = TokenManager.generate_token(
             account.role_type,
             account.role_id
@@ -52,43 +29,66 @@ class AccountServer(BaseManager):
         return token
 
     @classmethod
-    def is_exsited(cls,username):
-        account=cls.APPLY_CLS.get_byusername(username)
-        if account is None:
-            return False
-        return True
+    def renew_token(cls, auth_str, renew_str):
+        return TokenManager.renew_token(auth_str, renew_str)
 
     @classmethod
-    def forget_password(cls,phone,code,new_password):
-        account=cls.APPLY_CLS.get_byusername(phone)
-        if account is None:
-            raise BusinessError('账户不存在！')
+    def login(cls, username, password):
+        is_exsited, account = cls.APPLY_CLS.is_exsited(username, password)
+        if is_exsited and password:
+            token = TokenManager.generate_token(
+                account.role_type,
+                account.role_id
+            )
+            return token
+        raise BusinessError('账号或密码错误！')
 
-        account.update(password=new_password)
-        token=TokenManager.generate_token(
+    @classmethod
+    def account_login(cls,  account):
+        token = TokenManager.generate_token(
             account.role_type,
             account.role_id
         )
         return token
 
     @classmethod
-    def modify_password(cls,role_id,old_password,new_password):
-        account=cls.APPLY_CLS.get_byrole(role_id)
-        if account.password!=old_password:
+    def is_exsited(cls, username):
+        account = cls.APPLY_CLS.get_byusername(username)
+        if account is None:
+            return False
+        return True
+
+    @classmethod
+    def forget_password(cls, phone, code, new_password):
+        account = cls.APPLY_CLS.get_byusername(phone)
+        if account is None:
+            raise BusinessError('账户不存在！')
+
+        account.update(password=new_password)
+        token = TokenManager.generate_token(
+            account.role_type,
+            account.role_id
+        )
+        return token
+
+    @classmethod
+    def modify_password(cls, role_id, old_password, new_password):
+        account = cls.APPLY_CLS.get_byrole(role_id)
+        if account.password != old_password:
             raise BusinessError('原密码不正确，请重试！')
         account.update(password=new_password)
         return True
 
     @classmethod
-    def logout(cls,role_id):
-        pass
+    def logout(cls,  auth_str):
+        TokenManager.clear_token(auth_str)
 
     @classmethod
-    def get_byids(cls, role_id_list, limit=100):
+    def get_byids(cls,  role_id_list, limit=100):
         if len(role_id_list) > 100:
             raise BusinessError('账户搜索超过上限')
         account_qs = cls.APPLY_CLS.search(
-            role_id__in = role_id_list
+            role_id__in=role_id_list
         )
         return account_qs
 
@@ -97,7 +97,7 @@ class AccountServer(BaseManager):
         return "654321"
 
     @classmethod
-    def check_image_verification_code(cls,phone_number,code):
-        if code!="654321":
+    def check_image_verification_code(cls, phone_number, code):
+        if code != "654321":
             return False
         return True
--- src/abs/middleware/token/__init__.py
+++ src/abs/middleware/token/__init__.py
@@ -8,18 +8,23 @@ from abs.middleware.token.token import Token
 class TokenManager(object):
 
     @classmethod
-    def generate_token(cls,role,user_id):
+    def generate_token(cls, role, user_id):
         token=Token.generate(role,user_id)
         return token
 
     @classmethod
-    def renew_token(cls,auth_str,renew_str):
+    def renew_token(cls, auth_str, renew_str):
         token=Token.get(auth_str)
         token.renew(renew_str)
         return token
 
     @classmethod
-    def get_token(cls,auth_str,parms=None):
+    def get_token(cls, auth_str, parms=None):
         token=Token.get(auth_str)
         token.check(parms)
         return token
+
+    @classmethod
+    def clear_token(cls, auth_str):
+        token=Token.get(auth_str)
+        token.clear()
