a5038268f92fc9bd78dfd34c21f6aeb66461bd85
shiyufeng <452703041@qq.com>
2020-08-19 18:16:53 +0800

    modify bugs

--- support/test/projects/agent/api/customer/test_customer_chance.py
+++ support/test/projects/agent/api/customer/test_customer_chance.py
@@ -3,6 +3,7 @@
 import json
 
 from support.common.testcase.agent_api_test_case import AgentAPITestCase
+from support.common.generator.field.model.entity import ProductionEntity
 
 
 class CustomerChanceTestCase(AgentAPITestCase):
@@ -10,40 +11,43 @@ class CustomerChanceTestCase(AgentAPITestCase):
     def setUp(self):
         self.production = ProductionEntity().generate()
         self.sale_chance_info = {
-            'phone': '计算机学与技术' + str(random.randint(0, 100)),
-            'production_id': '',
-            'city':json.dumps([]),
-            'education':json.dumps([]),
-            'remark':'',
+            'name': '张三丰',
+            'phone': '13223090000',
+            'production_id': self.production.id,
+            'city':"湖北省武汉市洪山区",
+            'education':"middle",
+            'remark':'123131',
         }
 
     def tearDown(self):
         pass
 
-    def assert_customer_fields(self, customer, need_id = False):
+    def assert_chance_fields(self, chance, need_id = False):
         if need_id:
-            self.assertTrue('id' in customer)
-        self.assertTrue('nick' in customer)
-        self.assertTrue('head_url' in customer)
-        self.assertTrue('name' in customer)
-        self.assertTrue('gender' in customer)
-        self.assertTrue('birthday' in customer)
-        self.assertTrue('phone' in customer)
-        self.assertTrue('email' in customer)
-        self.assertTrue('wechat' in customer)
-        self.assertTrue('qq' in customer)
-        self.assertTrue('create_time' in customer)
+            self.assertTrue('id' in chance)
+        self.assertTrue('phone' in chance)
+        self.assertTrue('name' in chance)
+        self.assertTrue('wechat' in chance)
+        self.assertTrue('education' in chance)
+        self.assertTrue('production_id' in chance)
+        self.assertTrue('production_name' in chance)
+        self.assertTrue('city' in chance)
+        self.assertTrue('staff_id' in chance)
+        self.assertTrue('staff_name' in chance)
+        self.assertTrue('remark' in chance)
+        self.assertTrue('end_time' in chance)
+        self.assertTrue('create_time' in chance)
 
-
+    '''
     def test_create_chance(self):
         api = 'customer.salechance.add'
         self.access_api(
             api = api,
             sale_chance_info = json.dumps(self.sale_chance_info)
         )
-
+    '''
     def test_search_customer(self):
-        api = 'customer.search'
+        api = 'customer.salechance.search'
         current_page = 1
         result = self.access_api(
             api = api,
@@ -53,7 +57,6 @@ class CustomerChanceTestCase(AgentAPITestCase):
         self.assertTrue("data_list" in result)
         self.assertTrue("total" in result)
         self.assertTrue("total_page" in result)
-
-        for customer in result['data_list']:
-            self.assert_customer_fields(customer, True)
+        for chance in result['data_list']:
+            self.assert_chance_fields(chance, True)
         return result['data_list']
