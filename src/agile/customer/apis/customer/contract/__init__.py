d1cf072afc96ebab1456f846c756ce61dd8fa1e8
shiyufeng <452703041@qq.com>
2020-09-01 09:57:16 +0800

    modify bugs

--- src/agile/customer/apis/customer/contract/__init__.py
+++ src/agile/customer/apis/customer/contract/__init__.py
@@ -1,5 +1,6 @@
 # coding=UTF-8
 import json
+from urllib import parse
 from infrastructure.core.field.base import CharField, DictField, ListField, \
     IntField, DatetimeField
 from infrastructure.core.api.utils import with_metaclass
@@ -144,9 +145,12 @@ class Autograph(CustomerAuthorizedApi):
         contract = ContractServer.get(request.contract_id)
         order_item = OrderItemServer.get(contract.order_item_id)
         contract.order_item = order_item
+        autograph = parse.unquote(
+            request.contract_info.pop('autograph')
+        )
         ContractServer.autograph(
             contract,
-            request.contract_info['autograph'],
+            autograph,
             request.contract_info['email']
         )
 
